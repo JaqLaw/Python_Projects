f68077dffc63ce2a55cd7da6280d0e8677f79815
JaqLaw <93956690+JaqLaw@users.noreply.github.com>
2021-12-07 09:38:25 -0800

    class inheritance

---
+++ classes/class_inheritance.py
@@ -0,0 +1,16 @@
+
+
+class Animal:
+    species = 'No species name provided'
+    color = 'No color provided'
+    age = 0
+
+class Animal(Dog):
+    breed = 'No breed provided'
+    name = 'No name provided'
+    trained = False
+
+class Animal(Cat):
+    breed = 'No breed provided'
+    name = 'No name provided'
+    feral = False
